abdf92f13198a3a8bcf386af4981caf1ac9f6a16
Natã Araujo <natan.mca@gmail.com>
2023-03-17 19:38:41 -0300

    Feat: Menu responsive creation

---
+++ src/Components/Header/menu.tabs.ts
@@ -0,0 +1,19 @@
+type ListMenuType = {
+  title: string;
+  path: string;
+};
+
+export const ListMenu: ListMenuType[] = [
+  {
+    title: "HOME",
+    path: "/"
+  },
+  {
+    title: "PRODUTOS",
+    path: "/products"
+  },
+  {
+    title: "REVENDA",
+    path: "/resale"
+  }
+];
--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
@@ -5,27 +6,39 @@ export const Wrapper = styled.div`
   gap: 20px;
 `;
 
-export const Notice = styled.div`
+export const Notice = styled(Box)`
   display: flex;
   justify-content: center;
-  align-items: center;
-  height: 40px;
   background-color: #9ee384;
   font-size: 12px;
   font-weight: 600;
   color: #141414;
   letter-spacing: 3px;
+  padding: 5px;
+  p {
+    text-align: center;
+  }
 `;
 
-export const MenuHeader = styled.div`
+export const MenuHeader = styled(Box)`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  margin: 0 10px;
+  position: relative;
   ul {
     display: flex;
     gap: 30px;
     margin: 0;
     padding: 0;
+    @media only screen and (max-width: 600px) {
+      width: 100%;
+      flex-direction: column;
+      align-items: center;
+      margin-top: 25px;
+      order: 2;
+      gap: 15px;
+    }
   }
   li {
     list-style-type: none;
@@ -40,8 +53,40 @@ export const MenuHeader = styled.div`
       }
     }
   }
+  @media only screen and (max-width: 600px) {
+    justify-content: space-between;
+  }
 `;
 
-export const Logo = styled.img `
+export const Logo = styled.img`
   width: 130px;
-`
+`;
+
+export const BoxMenuResponse = styled(Box)<{ visible: boolean }>`
+  display: none;
+  transition: 0.5s;
+  right: ${(props) => (props.visible ? "0" : "-1000px")};
+  width: 100%;
+  @media only screen and (max-width: 600px) {
+    display: grid;
+    background-color: #cdfabb;
+    position: absolute;
+    height: 400px;
+    top: 25px;
+  }
+`;
+
+export const MenuDesktop = styled(Box)`
+  @media only screen and (max-width: 600px) {
+    display: none;
+  }
+`;
+
+export const BoxMenuIcon = styled(Box)`
+  display: none;
+  z-index: 999;
+  cursor: pointer;
+  @media only screen and (max-width: 600px) {
+    display: block;
+  }
+`;
